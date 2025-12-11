b420c6b6b2f01d33e1973c7cab4c82be3ebe95c2
Clark Wang <clarkywang09@gmail.com>
2025-12-11 15:35:31 -0600

    Chaned a to 2

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("Training Repo");
-        int a = 1;
+        int a = 2;
         int b = 1;
         int c = a + b;
         System.out.println("a + b = " + c);

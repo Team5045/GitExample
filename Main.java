4046309279d001aa35d053ea6670d4b3390bc456
Clark Wang <clarkywang09@gmail.com>
2025-12-15 15:07:59 -0600

    Added encapsulation, inheritance, polymorphism example (Animal & Cat)

--- Main.java
+++ Main.java
@@ -5,5 +5,14 @@ class Main{
         int b = 1;
         int c = a + b;
         System.out.println("a + b = " + c);
+
+        Animal a1 = new Animal(5, "roar");
+        System.out.println(a1.getAge());
+        a1.setAge(250);
+        System.out.println(a1.getAge());
+        Cat c1 = new Cat(4, "purr");
+
+        a1.talk();
+        c1.talk();
     }
 }
